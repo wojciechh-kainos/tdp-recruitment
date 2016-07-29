050c3fc10507854395a6152f4f3538fefd5b42b1
Sara Cyganska <sarac@giant.local>
2016-07-29 11:36:08 +0200

    Add Slots and SlotsDao

---
+++ src/main/java/dao/SlotsDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by sarac on 29/07/16.
+ */
+public class SlotsDao {
+}
---
+++ src/main/java/domain/Slots.java
@@ -0,0 +1,7 @@
+package domain;
+
+/**
+ * Created by sarac on 29/07/16.
+ */
+public class Slots {
+}
