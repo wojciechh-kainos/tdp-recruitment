a7d2f530da5376861ab99e615df6fca4faeebc70
mntss <mateusz.piotrowski94@gmail.com>
2016-08-03 15:39:57 +0200

    US View all slots: Add service tests and date params to backend call

--- src/main/resources/assets/js/test/personsServiceTest.js
+++ src/main/resources/assets/js/test/personsServiceTest.js
@@ -4,19 +4,31 @@ define(['angular', 'angularMocks', 'application/recruiter/services/personsServic
 
         var $httpBackend;
         var service;
-
+        var data = [2, 2];
 
         beforeEach(inject(function (_personsService_, _$httpBackend_) {
             $httpBackend = _$httpBackend_;
             service = _personsService_;
         }));
 
-        describe('getData', function () {
-            it('should return valid data', function () {
-                
-                expect(true).toEqual(true);
+        describe('fetchPersons', function () {
+            it('should call backend with date from current week', function () { //test should be updated later
+                $httpBackend.expectGET('api/person/all?startDate=2016-08-01&endDate=2016-08-07').respond(200);
 
+                service.fetchPersons().then(function (response) {
+                expect(response.status).toEqual(200);
+                });
+
+                $httpBackend.flush();
             });
+            it('should set service persons field', function () {
+                $httpBackend.expectGET('api/person/all?startDate=2016-08-01&endDate=2016-08-07').respond(200, data);
+
+                service.fetchPersons().then(function () {
+                    expect(service.getPersons()).toEqual(data);
+                });
+                $httpBackend.flush();
+            })
 
         })
     })
