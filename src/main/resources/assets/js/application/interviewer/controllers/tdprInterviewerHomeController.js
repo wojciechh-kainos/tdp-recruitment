aefec9ac0705118efb0b8da0f8fcdf606c9edd9c
Dawid Kwidzinski <dawidkw@gargoyle.local>
2016-08-03 14:37:34 +0200

    Revert "US-2 add html and controller for view"
    This reverts commit 2cc676c9f0a5e156c0a53842972852a08456c7d6.

--- src/main/resources/assets/js/application/interviewer/controllers/tdprInterviewerHomeController.js
+++
@@ -1,5 +0,0 @@
-define(['angular', 'application/interviewer/tdprInterviewerModule'], function(angular, tdprInterviewerModule) {
-  tdprInterviewerModule.controller("tdprInterviewerHomeController", function($scope) {
-
-  });
-});
--- src/main/resources/assets/js/application/tdprModule.js
+++ src/main/resources/assets/js/application/tdprModule.js
@@ -1,6 +1,5 @@
 define(['angular', 'uiRouter'
     , 'application/interviewer/tdprInterviewerModule'
-    , 'application/interviewer/tdprInterviewerModuleConfig'
     , 'application/recruiter/tdprRecruiterModule'
 ], function(angular) {
     return angular.module("tdprModule", ['ui.router', 'tdprInterviewerModule', 'tdprRecruiterModule']);
