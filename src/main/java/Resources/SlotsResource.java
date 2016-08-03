bdd68db2fa0d1fef7a26f0f958bd8f451cc4ade8
Remigiusz Kruk <r.kruk@kainos.com>
2016-08-03 15:04:10 +0200

    US-2 add resource file

---
+++ src/main/java/Resources/SlotsResource.java
@@ -0,0 +1,44 @@
+package Resources;
+
+/**
+ * Created by remigiuszk on 03/08/16.
+ */
+
+import dao.SlotsDao;
+import domain.Slots;
+import io.dropwizard.hibernate.UnitOfWork;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Path("/slots")
+public class SlotsResource {
+
+    SlotsDao dao;
+    @Inject
+    SlotsResource(SlotsDao dao){
+        this.dao = dao;
+    }
+
+    @GET
+    @Path("/week")
+    @UnitOfWork
+    List<Slots> fetchSlotsForWeek(@QueryParam("id") Long id,
+                                  @QueryParam("startDate") String startDate,
+                                  @QueryParam("endDate") String endDate) throws ParseException {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+
+        Date start = formatter.parse(startDate);
+        Date end = formatter.parse(endDate);
+
+        List<Slots> list = dao.getForPersonForWeek(id,start,end);
+        return list;
+    }
+}
