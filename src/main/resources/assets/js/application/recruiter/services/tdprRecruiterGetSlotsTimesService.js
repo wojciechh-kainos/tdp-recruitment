14590f720efc4162bfc07899a44d36824bcfa326
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-08-03 15:50:50 +0200

    US-1: Get slots times: Add get slots times service tests

--- src/main/resources/assets/js/application/recruiter/services/tdprRecruiterGetSlotsTimesService.js
+++ src/main/resources/assets/js/application/recruiter/services/tdprRecruiterGetSlotsTimesService.js
@@ -1,19 +1,16 @@
 define(['angular', 'application/recruiter/tdprRecruiterModule'], function (angular, tdprRecruiterModule) {
-    tdprRecruiterModule.service('tdprRecruiterGetSlotsTimesService', ['$http', function ($http) {
+    tdprRecruiterModule.service('tdprRecruiterGetSlotsTimesService', ['$http', '$q', function ($http, $q) {
         var service = {};
 
         service.getSlotsTimes = function(){
-            return $http.post('/api/slots_times/all').then(function(response){
-                console.log(response);
-                return response;
+            return $http.get('/api/slots_times/all').then(function(response){
+                return response.data;
             },
             function(error){
-                console.log(error);
-                console.log("get slots times failed");
-                return {};
+                error.message = "Failed";
+                return $q.reject(error.message);
             });
         }
-
         return service;
     }]);
 });
