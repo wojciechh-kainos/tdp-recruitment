06be184c23f99366b0f546dcae9f0844d2fe19a9
Remigiusz Kruk <r.kruk@kainos.com>
2016-08-04 15:31:57 +0200

    US-2 retreive slot times from database instead of harcoding

--- src/main/resources/assets/js/application/interviewer/controllers/tdprInterviewerHomeController.js
+++ src/main/resources/assets/js/application/interviewer/controllers/tdprInterviewerHomeController.js
@@ -1,7 +1,13 @@
 define(['angular', 'application/interviewer/tdprInterviewerModule'], function(angular, tdprInterviewerModule) {
   tdprInterviewerModule.controller("tdprInterviewerHomeController", function($scope, tdprSlotsService, $filter, $stateParams,$state,$timeout) {
-        $scope.slotTimes = ["9.00-9.30", "9.30-10.00", "10.00-10.30", "10.30-11.00", "11.00-11.30", "11.30-12.00", "12.00-12.30",
-         "12.30-13.00", "13.00-13.30", "13.30-14.00", "14.00-14.30", "14.30-15.00", "15.00-15.30", "15.30-16.00", "16.00-16.30","16.30-17.00", "17.00-17.30", "17.30-18.00"];
+    $scope.slotTimes = [];
+        tdprSlotsService.getSlotsTimes().then(function(response) {
+            for(var i = 0; i < response.data.length; i++) {
+                var startTime = response.data[i].startTime.slice(0,5);
+                var endTime = response.data[i].endTime.slice(0,5);
+                $scope.slotTimes.push(startTime + "-" + endTime);
+            }
+    });
 
         $scope.startDate;
         $scope.endDate;
