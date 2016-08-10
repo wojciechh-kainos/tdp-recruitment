03877096f676366c28d62f62d2580b9a795f613a
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-08-10 12:31:34 +0200

    US-5: Refactor filter

--- src/main/resources/assets/js/application/recruiter/filters/tdprRecruiterJobProfileFilter.js
+++ src/main/resources/assets/js/application/recruiter/filters/tdprRecruiterJobProfileFilter.js
@@ -1,33 +1,13 @@
 define(['application/recruiter/tdprRecruiterModule'], function (tdprRecruiterModule) {
     tdprRecruiterModule.filter('jobProfileFilter', function(){
         return function(persons, jobProfile){
-             var response = [];
-
              if(jobProfile == undefined || jobProfile == ""){
                 return persons;
              }
 
-             for(var i = 0; i < persons.length; i++){
-                 switch(jobProfile){
-                    case 'isDev': {
-                        if(persons[i].isDev == true)
-                            response.push(persons[i]);
-                            break;
-                    }
-                    case 'isOps': {
-                        if(persons[i].isOps == true)
-                            response.push(persons[i]);
-                            break;
-                    }
-                    case 'isTest': {
-                        if(persons[i].isTest == true)
-                            response.push(persons[i]);
-                            break;
-                    }
-                 }
-             }
-
-             return response;
+            return persons.filter(function(person){
+                return person[jobProfile];
+            })
         }
     });
 })
