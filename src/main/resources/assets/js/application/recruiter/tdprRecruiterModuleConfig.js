faa5f308220678a2d9c53dc5e1d3230320202d04
mntss <mateusz.piotrowski94@gmail.com>
2016-08-11 11:35:54 +0200

    Refactor: Css fixes

--- src/main/resources/assets/js/application/recruiter/tdprRecruiterModuleConfig.js
+++ src/main/resources/assets/js/application/recruiter/tdprRecruiterModuleConfig.js
@@ -24,10 +24,12 @@ define(['angular'
             resolve: {
                 persons: function (tdprPersonsService, tdprDateService) {
                     var week = tdprDateService.getCurrentWeek();
-                    return tdprPersonsService.fetchPersonsWithSlotsForDates(week[0], week[4]).data;
+                    return tdprPersonsService.fetchPersonsWithSlotsForDates(week[0], week[4]).then(function (response) {
+                        return response.data
+                    });
                 },
                 slotsTimes: function (tdprSlotsTimesService) {
-                    return tdprSlotsTimesService.fetchSlotsTimes().data;
+                    return tdprSlotsTimesService.fetchSlotsTimes();
                 }
             },
             views: {
