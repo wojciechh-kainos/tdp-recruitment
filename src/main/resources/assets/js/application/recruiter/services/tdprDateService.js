e5c449232112ffe293b688fd7c81f604769b290f
Marcin Trendel <m.trendel@kainos.com>
2016-08-11 16:23:44 +0200

    US-6: Remove unnecessary methods

--- src/main/resources/assets/js/application/recruiter/services/tdprDateService.js
+++ src/main/resources/assets/js/application/recruiter/services/tdprDateService.js
@@ -20,25 +20,5 @@ define(['application/recruiter/tdprRecruiterModule'], function (tdprRecruiterMod
             return weekDays;
         };
 
-        this.resetDate = function (dateToReset) {
-            var date = new Date(dateToReset);
-
-            // Compensate for timezone differences
-            date.setHours(date.getTimezoneOffset() / (-60.0));
-            date.setMinutes(0);
-            date.setSeconds(0);
-            date.setMilliseconds(0);
-
-            return date;
-        };
-
-        this.formatDate = function (dateToFormat) {
-            return dateFilter(dateToFormat, "yyyy-MM-dd");
-        };
-
-        this.compareTime = function (compare, compareTo) {
-            if (compare === undefined || compareTo === undefined) return false;
-            return compare.getTime() === compareTo.getTime();
-        };
-    })
+    });
 });
