f37990b82bf30e1ee8496f1e85b43f9daa159584
mntss <mateusz.piotrowski94@gmail.com>
2016-08-12 12:32:03 +0200

    Implement hours filter: Add hours filter to recruiter table

--- src/main/resources/assets/js/application/recruiter/controllers/tdprWeekTableController.js
+++ src/main/resources/assets/js/application/recruiter/controllers/tdprWeekTableController.js
@@ -1,13 +1,20 @@
 define(['angular', 'application/recruiter/tdprRecruiterModule'
     ], function (angular, tdprRecruiterModule) {
-    tdprRecruiterModule.controller("tdprWeekTableController", function ($scope, tdprPersonsService, tdprDateService, persons, slotsTimes, JobProfileEnum) {
+    tdprRecruiterModule.controller("tdprWeekTableController", function ($scope, tdprPersonsService, tdprDateService, persons, slotsTimes, JobProfileEnum, $filter) {
 
         $scope.JobProfileEnum = JobProfileEnum;
         $scope.currentJobProfile = JobProfileEnum.dev;
 
         $scope.days = tdprDateService.getCurrentWeek();
         $scope.slotsTimes = slotsTimes;
+        $scope.selectSlotsTimes = slotsTimes;
         $scope.persons = persons;
+        $scope.endTime = slotsTimes[slotsTimes.length - 1].endTime;
+        $scope.startTime = slotsTimes[0].startTime;
 
+
+        $scope.filterSlots = function () {
+            $scope.slotsTimes = $filter('slotsByTime')(slotsTimes, $scope.startTime, $scope.endTime);
+        }
     })
 });
