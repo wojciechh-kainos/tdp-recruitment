3f5de6038094539b37f775a8f59cb477d1302fb1
mntss <mateusz.piotrowski94@gmail.com>
2016-08-16 14:09:47 +0200

    Implement hours filter: Add test with undefined param

--- src/main/resources/assets/js/test/recruiter/slotsByTimeFilterTest.js
+++ src/main/resources/assets/js/test/recruiter/slotsByTimeFilterTest.js
@@ -12,25 +12,26 @@ define(['angular', 'application/recruiter/filters/tdprSlotsByTimeFilter', 'appli
             });
         });
 
+        var result, data = [{"id": 1, "startTime": "08:00:00", "endTime": "08:30:00"},
+            {"id": 2, "startTime": "08:30:00", "endTime": "09:00:00"},
+            {"id": 3, "startTime": "09:00:00", "endTime": "09:30:00"},
+            {"id": 4, "startTime": "09:30:00", "endTime": "10:00:00"},
+            {"id": 5, "startTime": "10:00:00", "endTime": "10:30:00"},
+            {"id": 6, "startTime": "10:30:00", "endTime": "11:00:00"},
+            {"id": 7, "startTime": "11:00:00", "endTime": "11:30:00"},
+            {"id": 8, "startTime": "11:30:00", "endTime": "12:00:00"},
+            {"id": 9, "startTime": "12:00:00", "endTime": "12:30:00"},
+            {"id": 10, "startTime": "12:30:00", "endTime": "13:00:00"},
+            {"id": 11, "startTime": "13:00:00", "endTime": "13:30:00"},
+            {"id": 12, "startTime": "13:30:00", "endTime": "14:00:00"},
+            {"id": 13, "startTime": "14:00:00", "endTime": "14:30:00"},
+            {"id": 14, "startTime": "14:30:00", "endTime": "15:00:00"},
+            {"id": 15, "startTime": "15:00:00", "endTime": "15:30:00"},
+            {"id": 16, "startTime": "15:30:00", "endTime": "16:00:00"},
+            {"id": 17, "startTime": "16:00:00", "endTime": "16:30:00"},
+            {"id": 18, "startTime": "16:30:00", "endTime": "17:00:00"}];
+
         it('should return only some of the slots', function () {
-            var result, data = [{"id": 1, "startTime": "08:00:00", "endTime": "08:30:00"},
-                {"id": 2, "startTime": "08:30:00", "endTime": "09:00:00"},
-                {"id": 3, "startTime": "09:00:00", "endTime": "09:30:00"},
-                {"id": 4, "startTime": "09:30:00", "endTime": "10:00:00"},
-                {"id": 5, "startTime": "10:00:00", "endTime": "10:30:00"},
-                {"id": 6, "startTime": "10:30:00", "endTime": "11:00:00"},
-                {"id": 7, "startTime": "11:00:00", "endTime": "11:30:00"},
-                {"id": 8, "startTime": "11:30:00", "endTime": "12:00:00"},
-                {"id": 9, "startTime": "12:00:00", "endTime": "12:30:00"},
-                {"id": 10, "startTime": "12:30:00", "endTime": "13:00:00"},
-                {"id": 11, "startTime": "13:00:00", "endTime": "13:30:00"},
-                {"id": 12, "startTime": "13:30:00", "endTime": "14:00:00"},
-                {"id": 13, "startTime": "14:00:00", "endTime": "14:30:00"},
-                {"id": 14, "startTime": "14:30:00", "endTime": "15:00:00"},
-                {"id": 15, "startTime": "15:00:00", "endTime": "15:30:00"},
-                {"id": 16, "startTime": "15:30:00", "endTime": "16:00:00"},
-                {"id": 17, "startTime": "16:00:00", "endTime": "16:30:00"},
-                {"id": 18, "startTime": "16:30:00", "endTime": "17:00:00"}];
 
 
             // Act.
@@ -39,10 +40,29 @@ define(['angular', 'application/recruiter/filters/tdprSlotsByTimeFilter', 'appli
             // Assert.
             expect(result).toEqual(
                 [{"id": 9, "startTime": "12:00:00", "endTime": "12:30:00"},
-                {"id": 10, "startTime": "12:30:00", "endTime": "13:00:00"},
-                {"id": 11, "startTime": "13:00:00", "endTime": "13:30:00"},
-                {"id": 12, "startTime": "13:30:00", "endTime": "14:00:00"}]
+                    {"id": 10, "startTime": "12:30:00", "endTime": "13:00:00"},
+                    {"id": 11, "startTime": "13:00:00", "endTime": "13:30:00"},
+                    {"id": 12, "startTime": "13:30:00", "endTime": "14:00:00"}]
             );
         });
+
+        it('should ignore constrains if params are undefined', function () {
+
+            result = $filter('slotsByTime')(data, '12:00:00');
+
+            expect(result).toEqual(
+                [{"id": 9, "startTime": "12:00:00", "endTime": "12:30:00"},
+                    {"id": 10, "startTime": "12:30:00", "endTime": "13:00:00"},
+                    {"id": 11, "startTime": "13:00:00", "endTime": "13:30:00"},
+                    {"id": 12, "startTime": "13:30:00", "endTime": "14:00:00"},
+                    {"id": 13, "startTime": "14:00:00", "endTime": "14:30:00"},
+                    {"id": 14, "startTime": "14:30:00", "endTime": "15:00:00"},
+                    {"id": 15, "startTime": "15:00:00", "endTime": "15:30:00"},
+                    {"id": 16, "startTime": "15:30:00", "endTime": "16:00:00"},
+                    {"id": 17, "startTime": "16:00:00", "endTime": "16:30:00"},
+                    {"id": 18, "startTime": "16:30:00", "endTime": "17:00:00"}]
+            );
+
+        })
     });
 });
