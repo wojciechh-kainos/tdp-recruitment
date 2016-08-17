c7373cad53a5826926455b88966c3c9cbc682cc6
Marcin Trendel <m.trendel@kainos.com>
2016-08-17 12:25:06 +0200

    US-34: Add filter

--- src/main/resources/assets/js/application/report/filters/tdprReportByPersonNameFilter.js
+++ src/main/resources/assets/js/application/report/filters/tdprReportByPersonNameFilter.js
@@ -1,10 +1,15 @@
 define(['application/report/tdprReportModule'], function (tdprReportModule) {
     tdprReportModule.filter('personNameFilter', function(){
         return function(reports, phrase){
-            var words = str.split(phrase);
-            var response = [];
-            return _.filter(reports, function(value, key){
-                return _.includes(values.person.firstName, phrase) || _.includes(values.person.lastName, phrase);
+            if ( phrase === undefined ) {
+                return reports;
+            }
+            var searchPhrase = phrase.toLowerCase();
+            return _.filter(reports, function(value){
+                var firstName = value.person.firstName.toLowerCase();
+                var lastName = value.person.lastName.toLowerCase();
+
+                return _.includes(firstName, searchPhrase) || _.includes(lastName, searchPhrase) || _.includes(firstName + ' ' + lastName, searchPhrase) || _.includes(lastName + ' ' + firstName, searchPhrase);
             })
         }
     });
