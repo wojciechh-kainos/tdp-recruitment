ba6de8eef43d2d4945d78b3b43a7b5ff6f2dfcb3
radoslawlandowskikainos <radoslawl@kainos.com>
2016-08-17 12:23:04 +0200

    US-34 Modify fixture test data, repair test

--- src/test/java/servicesTests/ReportServiceGetReportTest.java
+++ src/test/java/servicesTests/ReportServiceGetReportTest.java
@@ -9,6 +9,7 @@ import domain.Slots;
 import io.dropwizard.jackson.Jackson;
 import org.joda.time.LocalDate;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,6 +18,7 @@ import services.ReportService;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
@@ -28,6 +30,7 @@ public class ReportServiceGetReportTest {
 
     private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
     private Persons mockPerson;
+    private List<Persons> mockPersons;
     private List<Slots> mockSlotsList;
     private ReportService reportService;
     private Report expectedReport;
@@ -40,12 +43,19 @@ public class ReportServiceGetReportTest {
 
     @Before
     public void setUp() throws IOException {
-        mockPerson = MAPPER.readValue(fixture("fixtures/persons.json"), Persons.class);
+        mockPersons = MAPPER.readValue(fixture("fixtures/persons.json"),
+                MAPPER.getTypeFactory().constructCollectionType(List.class, Persons.class));
+        mockPerson = mockPersons.get(0);
         mockSlotsList = MAPPER.readValue(fixture("fixtures/slots.json"),
                 MAPPER.getTypeFactory().constructCollectionType(List.class, Slots.class));
 
         expectedReport = new Report(mockPerson, 0L,0L,0L);
         reportService = new ReportService(slotsDao, personsDao);
+
+        List<Report> expectedReports = new ArrayList<>();
+
+//        for(Persons p : mockPersons)
+//        expectedReports.add(new Report(mockPersons.get(0).));
 
     }
 
@@ -59,6 +69,19 @@ public class ReportServiceGetReportTest {
 
         Report report = reportService.getReport(mockPerson.getId(), startDate, endDate);
         assertThat(report.equals(expectedReport));
+    }
+
+    @Test
+    @Ignore
+    public void getAllReportsTest() {
+        Date startDate = null;
+        Date endDate = null;
+
+        when(personsDao.findAll()).thenReturn(mockPersons);
+        when(slotsDao.getForPersonForWeek(mockPerson.getId(), startDate, endDate)).thenReturn(mockSlotsList);
+
+        List<Report> achieved = reportService.getAllReports(startDate, endDate);
+        assertThat(achieved.equals(expectedReport));
     }
 
 
