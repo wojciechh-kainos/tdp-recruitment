67c98f2845f93f9817882848ead612416e5eab8f
Remigiusz Kruk <r.kruk@kainos.com>
2016-08-22 15:47:44 +0200

    US-41 change manageUsers view

--- src/main/resources/assets/js/application/recruiter/controllers/tdprManageUsersController.js
+++ src/main/resources/assets/js/application/recruiter/controllers/tdprManageUsersController.js
@@ -4,16 +4,17 @@ define(['angular', 'application/recruiter/tdprRecruiterModule'
 
         tdprPersonsService.fetchPersons().then( function (response) {
             $scope.persons = response.data;
-        }, function (error) {
-            return error.data;
+        }, function () {
+            Notification.error({message: "Something went wrong with getting users" , delay: 3500});
         });
 
         $scope.deactivatePerson = function( person ){
             person.active = !person.active;
-            tdprPersonsService.deactivatePerson(person).then( function (response) {
-                console.log("deactivated");
-            }, function (error) {
-                console.log("error");
+            tdprPersonsService.deactivatePerson(person).then( function () {
+                if(!person.active) Notification.success({message: person.firstName + " " + person.lastName  + " has been disabled." , delay: 3500});
+                else Notification.success({message: person.firstName + " " + person.lastName  + " has been enabled." , delay: 3500});
+            }, function () {
+                Notification.error({message: "Something went wrong with your request." , delay: 3500});
             });
         }
     });
