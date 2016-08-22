d622d53e8eec0f0eff7d3143ce875e5390da3fac
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-08-22 14:44:01 +0200

    Remove logic from MailingThread

--- src/main/java/services/MailingThread.java
+++ src/main/java/services/MailingThread.java
@@ -24,11 +24,13 @@ public class MailingThread extends Thread {
     private TdpRecruitmentApplicationConfiguration config;
     private String recipient;
     private Long personId;
+    private Message msg;
 
-    public MailingThread(TdpRecruitmentApplicationConfiguration config, String recipient, Long id) {
+    public MailingThread(TdpRecruitmentApplicationConfiguration config, String recipient, Long id, Message msg) {
         this.config = config;
         this.recipient = recipient;
         this.personId = id;
+        this.msg = msg;
 
     }
 
@@ -50,29 +52,9 @@ public class MailingThread extends Thread {
 
         Session session = Session.getDefaultInstance(props);
 
-
-        MailcapCommandMap mailcap = (MailcapCommandMap) MailcapCommandMap.getDefaultCommandMap();
-        mailcap.addMailcap("text/calendar;; x-java-content-handler=com.sun.mail.handlers.text_plain");
-
-
-
         try {
-            MimeMessage msg = new MimeMessage(session);
             msg.setFrom(new InternetAddress(from));
-            msg.setRecipient(Message.RecipientType.TO, new InternetAddress(this.recipient));
-            msg.setSubject("TDP Recruitment - Activation Link");
 
-            Multipart multipart = new MimeMultipart("alternative");
-
-            BodyPart messageBodyPart = buildHtmlTextPart();
-            multipart.addBodyPart(messageBodyPart);
-
-            BodyPart calendarPart = buildCalendarPart();
-            multipart.addBodyPart(calendarPart);
-
-            msg.setContent(multipart);
-
-            // send the message
             Transport transport = session.getTransport("smtp");
             transport.connect();
             transport.sendMessage(msg, msg.getAllRecipients());
@@ -88,78 +70,6 @@ public class MailingThread extends Thread {
             e.printStackTrace();
         }
 
-//        URL url = Resources.getResource("email/template.html");
-//        String text = null;
-//        try {
-//            text = Resources.toString(url, Charsets.UTF_8);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        String tempText = text.replace("{{domain}}", domain);
-//        String finalText = tempText.replace("{{id}}", personId.toString());
-//        email.setTextHTML(finalText);
-//        new Mailer(host, port, from, pass, TransportStrategy.SMTP_TLS).sendMail(email);
-
-    }
-
-    private BodyPart buildHtmlTextPart() throws MessagingException {
-
-        MimeBodyPart descriptionPart = new MimeBodyPart();
-
-        //Note: even if the content is specified as being text/html, outlook won't read correctly tables at all
-        // and only some properties from div:s. Thus, try to avoid too fancy content
-        String content = "bla";
-        descriptionPart.setContent(content, "text/html; charset=utf-8");
-
-        return descriptionPart;
-    }
-
-    //define somewhere the icalendar date format
-    private static SimpleDateFormat iCalendarDateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmm'00'");
-
-    private BodyPart buildCalendarPart() throws Exception {
-
-        BodyPart calendarPart = new MimeBodyPart();
-
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 1);
-        Date start = cal.getTime();
-        cal.add(Calendar.HOUR_OF_DAY, 3);
-        Date end = cal.getTime();
-
-        //check the icalendar spec in order to build a more complicated meeting request
-        String calendarContent =
-                "BEGIN:VCALENDAR\n" +
-                        "METHOD:REQUEST\n" +
-                        "PRODID: BCP - Meeting\n" +
-                        "VERSION:2.0\n" +
-                        "BEGIN:VEVENT\n" +
-                        "DTSTAMP:" + iCalendarDateFormat.format(start) + "\n" +
-                        "DTSTART:" + iCalendarDateFormat.format(start)+ "\n" +
-                        "DTEND:"  + iCalendarDateFormat.format(end)+ "\n" +
-                        "SUMMARY:test request\n" +
-                        "UID:324\n" +
-                        "ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE:MAILTO:m.piotrowski@kainos.com\n" +
-                        "ORGANIZER:MAILTO:m.piotrowski@kainos.com\n" +
-                        "LOCATION:on the net\n" +
-                        "DESCRIPTION:learn some stuff\n" +
-                        "SEQUENCE:0\n" +
-                        "PRIORITY:5\n" +
-                        "CLASS:PUBLIC\n" +
-                        "STATUS:CONFIRMED\n" +
-                        "TRANSP:OPAQUE\n" +
-                        "BEGIN:VALARM\n" +
-                        "ACTION:DISPLAY\n" +
-                        "DESCRIPTION:REMINDER\n" +
-                        "TRIGGER;RELATED=START:-PT00H15M00S\n" +
-                        "END:VALARM\n" +
-                        "END:VEVENT\n" +
-                        "END:VCALENDAR";
-
-        calendarPart.addHeader("Content-Class", "urn:content-classes:calendarmessage");
-        calendarPart.setContent(calendarContent, "text/calendar;method=CANCEL");
-
-        return calendarPart;
     }
 
 }
