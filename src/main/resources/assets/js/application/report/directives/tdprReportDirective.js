ef7500804f152914e8569c49b353eae43d14b32f
radoslawlandowskikainos <radoslawl@kainos.com>
2016-08-22 13:21:41 +0200

    US40 Replace dots with commas for better csv export

--- src/main/resources/assets/js/application/report/directives/tdprReportDirective.js
+++ src/main/resources/assets/js/application/report/directives/tdprReportDirective.js
@@ -4,6 +4,11 @@ define(['angular', 'application/report/tdprReportModule'], function (angular, td
             restrict: 'AE',
             templateUrl: 'js/application/report/views/tdpr-directive-report.html',
             replace: false,
+            link: function(scope) {
+                scope.reportData.initHours = scope.reportData.initHours.toString().replace(".", ",");
+                scope.reportData.fullHours = scope.reportData.fullHours.toString().replace(".", ",");
+                scope.reportData.availableHours = scope.reportData.availableHours.toString().replace(".", ",");
+            },
             scope: {
                 reportData: '=',
                 personInfo: '='
