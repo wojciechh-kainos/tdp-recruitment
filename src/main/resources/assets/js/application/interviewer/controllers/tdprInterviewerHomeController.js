b7d215e87813aeec1518361fa69f61a4bffed0ef
Daniel Rauf <danielr@kainos.com>
2016-08-22 11:56:23 +0200

    Ask to save or discard before going back to recruiter view

--- src/main/resources/assets/js/application/interviewer/controllers/tdprInterviewerHomeController.js
+++ src/main/resources/assets/js/application/interviewer/controllers/tdprInterviewerHomeController.js
@@ -30,14 +30,14 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
         };
 
         function isEligibleToEdit() {
-          return $scope.relativeDayNumber < 0 && !$scope.isRecruiter;
+            return $scope.relativeDayNumber < 0 && !$scope.isRecruiter;
         }
 
         function updateDate() {
             startDate = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber), "dd-MM-yyyy");
-            endDate  = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber + 4), "dd-MM-yyyy");
-            $scope.displayedStartDate = startDate.replace(/-/g,'.');
-            $scope.displayedEndDate = endDate.replace(/-/g,'.');
+            endDate = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber + 4), "dd-MM-yyyy");
+            $scope.displayedStartDate = startDate.replace(/-/g, '.');
+            $scope.displayedEndDate = endDate.replace(/-/g, '.');
         }
 
         $scope.getSlots = function (personId) {
@@ -53,10 +53,10 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
         function activate() {
             $scope.clearTable();
             updateDate();
-            tdprSlotsService.getSlotsTimes().then(function(response) {
-                for(var i = 0; i < response.data.length; i++) {
-                    var startTime = response.data[i].startTime.slice(0,5);
-                    var endTime = response.data[i].endTime.slice(0,5);
+            tdprSlotsService.getSlotsTimes().then(function (response) {
+                for (var i = 0; i < response.data.length; i++) {
+                    var startTime = response.data[i].startTime.slice(0, 5);
+                    var endTime = response.data[i].endTime.slice(0, 5);
                     $scope.slotTimes.push(startTime + "-" + endTime);
                 }
             });
@@ -66,43 +66,47 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
             getNote(id, startDate);
         }
 
-        $scope.goBackToRecruiterView = function(){
+        $scope.goBackToRecruiterView = function () {
+            if (!verifyNoUnsavedChanges()) {
+                return;
+            }
+
             $state.go('tdpr.recruiter.home');
         };
 
-        $scope.discardChanges = function() {
+        $scope.discardChanges = function () {
             $scope.clearTable();
             $scope.getSlots(id);
             startDate = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber), "dd-MM-yyyy");
             getNote(id, startDate);
-            $scope.hasSlotChanged=false;
-            $scope.hasNoteChanged=false;
+            $scope.hasSlotChanged = false;
+            $scope.hasNoteChanged = false;
         };
 
-        $scope.changeSlotStatus = function() {
-            if(isEligibleToEdit()){
+        $scope.changeSlotStatus = function () {
+            if (isEligibleToEdit()) {
                 Notification.error({message: 'You cannot edit slots from past weeks!', delay: 2000});
                 return;
             }
             $scope.hasSlotChanged = true;
         };
 
-        $scope.editNoteSwitch = function() {
+        $scope.editNoteSwitch = function () {
             if (isEligibleToEdit()) {
-              Notification.error({message: 'You cannot edit note from past weeks!', delay: 2000});
-              return;
+                Notification.error({message: 'You cannot edit note from past weeks!', delay: 2000});
+                return;
             }
-            if($scope.editNote) {
+            if ($scope.editNote) {
                 disableNoteEditing();
             } else {
                 enableNoteEditing();
             }
         };
 
-       $scope.showPreviousWeek = function() {
-             if(!verifyNoUnsavedChanges()){
+        $scope.showPreviousWeek = function () {
+            if (!verifyNoUnsavedChanges()) {
                 return;
-             }
+            }
 
             disableNoteEditing(); // set note input to disabled by default when changing weeks
 
@@ -115,8 +119,8 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
             getNote(id, startDate);
         };
 
-        $scope.showNextWeek = function() {
-            if(!verifyNoUnsavedChanges()){
+        $scope.showNextWeek = function () {
+            if (!verifyNoUnsavedChanges()) {
                 return;
             }
 
@@ -139,8 +143,8 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
 
         $scope.updateSlots = function () {
             if (isEligibleToEdit()) {
-              Notification.error({message: 'You cannot edit slots from past weeks!', delay: 2000});
-              return;
+                Notification.error({message: 'You cannot edit slots from past weeks!', delay: 2000});
+                return;
             }
             var slots = [];
             for (var i = 0; i < $scope.slotsForWeek.length; i++) {
@@ -148,7 +152,7 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
                     if ($scope.slotsForWeek[i][j].type !== AvailabilityEnum.empty.id) {
                         var slot = {
                             slotsDate: getDayOfTheWeek(new Date(), j + $scope.relativeDayNumber),
-                            person: {id:id},
+                            person: {id: id},
                             slot: {id: i + 1},
                             type: {id: $scope.slotsForWeek[i][j].type}
                         };
@@ -185,40 +189,41 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
         }
 
         function sendNote(note) {
-            tdprPersonService.updateNote(note).then(function(response) {
+            tdprPersonService.updateNote(note).then(function (response) {
                 $scope.temporaryContent = response.data.description;
                 $scope.noteContent = response.data;
-            }, function(failure) {
-                if(failure.status === 406) {
+            }, function (failure) {
+                if (failure.status === 406) {
                     $scope.temporaryContent = $scope.noteContent.description;
                 }
                 Notification.warning({
                     message: 'Something went wrong with sending your note.',
-                    delay: 2000});
+                    delay: 2000
+                });
             });
         }
 
-        $scope.markSlots = function(slot) {
-            if(!$scope.isRecruiter) {
-                if(isSlotTypeFullOrInit(slot) || $scope.relativeDayNumber < 0) {
+        $scope.markSlots = function (slot) {
+            if (!$scope.isRecruiter) {
+                if (isSlotTypeFullOrInit(slot) || $scope.relativeDayNumber < 0) {
                     return;
                 }
             }
 
-            slot.type === $scope.currentType ? slot.type = AvailabilityEnum.empty.id : slot.type = $scope.currentType
+            slot.type = (slot.type === $scope.currentType) ? AvailabilityEnum.empty.id : $scope.currentType
         };
 
         function isSlotTypeFullOrInit(slot) {
             return slot.type === AvailabilityEnum.full.id || slot.type === AvailabilityEnum.init.id;
         }
 
-        $scope.goDetails = function(){
-              $state.go('tdpr.interviewer.details', {'id' : id});
+        $scope.goDetails = function () {
+            $state.go('tdpr.interviewer.details', {'id': id});
         };
 
         function createNote(description, personId, date) {
             $scope.noteContent = {
-                description : description,
+                description: description,
                 person: {
                     id: personId
                 },
@@ -230,58 +235,63 @@ define(['angular', 'application/interviewer/tdprInterviewerModule', 'application
 
         function getNote(personId, date) {
             createNote("", personId, date);
-            tdprPersonService.getNote(personId, date).then(function(response) {
-                if(response.status === 200) {
+            tdprPersonService.getNote(personId, date).then(function (response) {
+                    if (response.status === 200) {
+                        $scope.noteContent = response.data;
+                        $scope.temporaryContent = response.data.description;
+                    }
+                }, function (failure) {
+                    Notification.warning({
+                        message: 'Something went wrong with getting your note.',
+                        delay: 2000
+                    });
+                }
+            )
+        }
+
+        $scope.getNoteFromPreviousWeek = function () {
+            if (isEligibleToEdit()) {
+                Notification.error({message: 'You cannot edit note from past weeks!', delay: 2000});
+                return;
+            }
+            var previousDate = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber - 7), "dd-MM-yyyy");
+            tdprPersonService.getNote(id, previousDate).then(function (response) {
+                if (response.status === 200) {
                     $scope.noteContent = response.data;
                     $scope.temporaryContent = response.data.description;
+                    Notification.warning("Please remember to submit your note.");
+                    if (response.data.description === "") Notification.warning("There was no content last week.");
+                    enableNoteEditing();
                 }
-            }, function(failure) {
+                else if (response.status === 204) {
+                    Notification.warning("You didn't submit any notes last week.")
+                } else {
+                    Notification.warning({
+                        message: 'Something went wrong.',
+                        delay: 2000
+                    });
+                }
+            }, function (failure) {
                 Notification.warning({
-                   message: 'Something went wrong with getting your note.',
-                   delay: 2000});
-               }
-        )}
-
-        $scope.getNoteFromPreviousWeek = function() {
-          if (isEligibleToEdit()) {
-            Notification.error({message: 'You cannot edit note from past weeks!', delay: 2000});
-            return;
-          }
-          var previousDate = $filter('date')(getDayOfTheWeek(new Date(), $scope.relativeDayNumber - 7 ), "dd-MM-yyyy");
-          tdprPersonService.getNote(id, previousDate).then(function(response) {
-               if(response.status === 200) {
-                  $scope.noteContent = response.data;
-                  $scope.temporaryContent = response.data.description;
-                  Notification.warning("Please remember to submit your note.");
-                  if(response.data.description === "") Notification.warning("There was no content last week.");
-                  enableNoteEditing();
-              }
-              else if (response.status === 204){
-                  Notification.warning("You didn't submit any notes last week.")
-              } else {
-                   Notification.warning({
-                      message: 'Something went wrong.',
-                      delay: 2000});
-              }
-          },function(failure) {
-                 Notification.warning({
                     message: 'Something went wrong with getting your note.',
-                    delay: 2000});
-          });
+                    delay: 2000
+                });
+            });
         };
 
         function verifyNoUnsavedChanges() {
-            if($scope.hasNoteChanged || $scope.hasSlotChanged) {
+            if ($scope.hasNoteChanged || $scope.hasSlotChanged) {
                 Notification.warning({
                     message: 'You have changed your data. Submit or discard your changes!',
-                    delay: 2000});
+                    delay: 2000
+                });
                 return false;
             } else return true;
         }
 
-        $scope.getClass = function(typeId) {
+        $scope.getClass = function (typeId) {
             for (var type in $scope.AvailabilityEnum) {
-                if($scope.AvailabilityEnum[type].id == typeId) {
+                if ($scope.AvailabilityEnum[type].id == typeId) {
                     return $scope.AvailabilityEnum[type].className;
                 }
             }
