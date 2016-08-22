5b79a44962fae3ea435f0eb3d42339b3286f2b07
msolecka <m.solecka@kainos.com>
2016-08-22 12:33:08 +0200

    Delete available hours from report, add sum of inits and fulls, delete unused getReport method

--- src/main/resources/assets/js/application/report/services/tdprReportService.js
+++ src/main/resources/assets/js/application/report/services/tdprReportService.js
@@ -2,23 +2,12 @@ define(['application/report/tdprReportModule'], function (tdprReportModule) {
     tdprReportModule.service('tdprReportService', ['$http', '$q', 'dateFilter', function ($http, $q, dateFilter) {
         var format = 'dd-MM-yyyy';
 
-        this.getReport = function (dateFrom, dateTo, personId) {
-            return $http.get('/api/report/' + dateFilter(dateFrom, format) + '/' + dateFilter(dateTo, format) + '/' + personId)
-                .then(
-                    function (response) {
-                        return response.data;
-                    },
-                    function (error) {
-                        error.message = "Unable to get data from server!";
-                        return $q.reject(error.message)
-                    })
-        };
-
         this.getReports = function (dateFrom, dateTo) {
             return $http.get('/api/report/' + dateFilter(dateFrom, format) + '/' + dateFilter(dateTo, format))
                 .then(
                     function (response) {
-                        return response.data;
+                         var reports = response.data.map(sumOfHours);
+                         return reports;
                     },
                     function (error) {
                         error.message = "Unable to get data from server!";
@@ -26,5 +15,9 @@ define(['application/report/tdprReportModule'], function (tdprReportModule) {
                     })
         };
 
+        var sumOfHours = function(item){
+            item.sumOfHours = item.initHours + item.fullHours;
+            return item;
+        }
     }])
 });
