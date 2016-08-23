a8392b19b52b48dacd64e509e147a54c841ea72d
msolecka <m.solecka@kainos.com>
2016-08-23 15:29:14 +0200

    US40 remove unused lines

--- src/main/resources/assets/js/application/report/directives/tdprReportDirective.js
+++ src/main/resources/assets/js/application/report/directives/tdprReportDirective.js
@@ -4,11 +4,6 @@ define(['angular', 'application/report/tdprReportModule'], function (angular, td
             restrict: 'AE',
             templateUrl: '/html/partials/report/tdpr-directive-report.html',
             replace: false,
-//            link: function(scope) {
-//                scope.reportData.initHours = scope.reportData.initHours.toString().replace(".", ",");
-//                scope.reportData.fullHours = scope.reportData.fullHours.toString().replace(".", ",");
-//                scope.reportData.availableHours = scope.reportData.availableHours.toString().replace(".", ",");
-//            },
             scope: {
                 reportData: '=',
                 personInfo: '='
--- src/main/resources/assets/js/application/require-config.js
+++ src/main/resources/assets/js/application/require-config.js
@@ -10,7 +10,7 @@ require.config({
         'lodash': 'lib/lodash/lodash',
         'notification': 'lib/angular-ui-notification/dist/angular-ui-notification',
         'moment' : 'lib/moment/moment',
-        'angular-moment' : 'lib/angular-moment/angular-moment',
+        'angular-moment' : 'lib/angular-moment/angular-moment'
     },
     shim: {
         'angular': {
@@ -23,7 +23,7 @@ require.config({
         'uiRouter': ['angular'],
         'notification': ['angular'],
         'moment' : ['angular'],
-        'angular-moment' : ['angular', 'moment'],
+        'angular-moment' : ['angular', 'moment']
     },
     deps: ['application/bootstrap']
 });
