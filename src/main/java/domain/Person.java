1962a137fc62eda8ab45f558c012c16ee0d53e20
Dawid Kręczkowski <d.kreczkowski@kainos.com>
2016-08-23 12:28:31 +0200

    US-51 enable updating password (backend)

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -2,6 +2,7 @@ package domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -36,7 +37,7 @@ public class Person implements Cloneable {
     @Length(max = 35)
     private String lastName;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @JsonIgnore
