7e66fdea9d3d7f3e813e23e01ade936db50355d8
Mateusz Czarnowski <m.czarnowski@kainos.com>
2016-08-23 16:28:02 +0200

    Move checkboxes to the right of person's name Add cancel button to createEvent view Add notification when in interview creation mode

--- src/main/resources/assets/js/application/recruiter/controllers/tdprCreateEventController.js
+++ src/main/resources/assets/js/application/recruiter/controllers/tdprCreateEventController.js
@@ -1,6 +1,6 @@
 define(['angular', 'application/recruiter/tdprRecruiterModule'
     , 'application/recruiter/services/tdprPersonsService'], function (angular, tdprRecruiterModule) {
-    tdprRecruiterModule.controller("tdprCreateEventController", function ($scope, tdprPersonsService, $stateParams) {
+    tdprRecruiterModule.controller("tdprCreateEventController", function ($scope, tdprPersonsService, $stateParams, $state) {
 
     console.log($stateParams);
 
@@ -10,6 +10,8 @@ define(['angular', 'application/recruiter/tdprRecruiterModule'
         $scope.startTime = $stateParams.data.start;
         $scope.endTime = $stateParams.data.end;
 
-
+        $scope.goHome = function () {
+            $state.go('tdpr.recruiter.home');
+        };
     });
 });
