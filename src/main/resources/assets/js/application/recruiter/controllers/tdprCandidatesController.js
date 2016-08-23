7d10d5f53bbc42ac1cb14a6dfd0ab3513eb22e60
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-08-23 12:15:51 +0200

    US-36: Add frontend: table sort

--- src/main/resources/assets/js/application/recruiter/controllers/tdprCandidatesController.js
+++ src/main/resources/assets/js/application/recruiter/controllers/tdprCandidatesController.js
@@ -2,12 +2,20 @@ define(['angular', 'application/recruiter/tdprRecruiterModule'
     , 'application/recruiter/services/tdprCandidatesService'], function (angular, tdprRecruiterModule) {
     tdprRecruiterModule.controller("tdprCandidatesController", function ($scope, tdprCandidatesService, candidates) {
 
-
             $scope.candidates = candidates;
+            $scope.sortColumn = 'lastName';
+            $scope.sortReverse = true;
 
+            $scope.columnMap = {
+                'lastName': {reverse: true, columnName: "Candidate"},
+                'position': {reverse: true, columnName: "Position"},
+                'note' : {reverse : true, columnName : "Note"},
+                'recruiter.lastName' : {reverse : true, columnName: "Recruiter"}
+            };
 
-
-
-
+            $scope.sortBy = function(column){
+                $scope.sortColumn = column;
+                $scope.sortReverse = $scope.columnMap[column].reverse = !$scope.columnMap[column].reverse;
+            }
     });
 });
