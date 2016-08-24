ba5fa5a6a3aaea9a72c18b2a5d3e0611ed41be47
Dawid Kręczkowski <d.kreczkowski@kainos.com>
2016-08-24 15:43:50 +0200

    US-51 refactor

--- src/main/resources/assets/js/application/common/directives/tdprNewPasswordDirective.js
+++ src/main/resources/assets/js/application/common/directives/tdprNewPasswordDirective.js
@@ -4,15 +4,16 @@ define(['angular', 'application/common/tdprCommonModule'], function (angular, td
             restrict: 'AE',
             templateUrl: '/html/partials/common/tdpr-directive-new-password.html',
             scope: {
-                arePasswordsCorrect: '='
+                arePasswordsCorrect: '=',
+                password: '='
             },
             link: function (scope) {
                 scope.arePasswordsDifferent = false;
                 scope.isPasswordValid = true;
-                scope.newPassword = '';
+                scope.password = '';
                 scope.confirmPassword = '';
 
-                scope.$watch('[newPassword, confirmPassword]', function (newValue, oldValue, scope) {
+                scope.$watch('[password, confirmPassword]', function (newValue, oldValue, scope) {
                     scope.arePasswordsDifferent = newValue[0] !== newValue[1];
                     scope.isPasswordValid = isPasswordValid(newValue[0]);
                     scope.arePasswordsCorrect = scope.isPasswordValid && !scope.arePasswordsDifferent;
