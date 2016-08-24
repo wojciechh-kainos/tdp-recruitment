4d52526ef6fa8c542c743f474f8f841fae889f36
radoslawlandowskikainos <radoslawl@kainos.com>
2016-08-24 10:43:26 +0200

    US40 Add dates to CSV file, move 'GenerateCSV' button near the table

--- src/main/resources/assets/js/application/report/controllers/tdprReportDetailsController.js
+++ src/main/resources/assets/js/application/report/controllers/tdprReportDetailsController.js
@@ -61,9 +61,9 @@ define(['angular', 'application/report/tdprReportModule'
 
         $scope.activate();
 
-        $scope.generateCSV = function() {
+        $scope.generateCSV = function(startDate, endDate) {
             var reportsForCSV = getReportsInActualOrder();
-            var data = tdprReportCsvDataService.generateCsvData(reportsForCSV, $scope.columnMap);
+            var data = tdprReportCsvDataService.generateCsvData(startDate, endDate, reportsForCSV, $scope.columnMap);
             $scope.downloadCsvFile(tdprReportCsvDataService.getLink(data));
         };
 
--- src/main/resources/assets/js/test/report/tdprReportCsvDataServiceTest.js
+++ src/main/resources/assets/js/test/report/tdprReportCsvDataServiceTest.js
@@ -26,6 +26,9 @@ define(['angular', 'angularMocks', 'application/report/services/tdprReportCsvDat
             }
         ]
 
+        var startDate = '2016-08-15';
+        var endDate = '2016-08-21';
+
         var columnMap = {
             'person.lastName': {reverse: true, columnName: "Person"},
             'initHours': {reverse: true, columnName: "Init hours"},
@@ -33,11 +36,15 @@ define(['angular', 'angularMocks', 'application/report/services/tdprReportCsvDat
             'sumOfHours': {reverse: true, columnName: "Sum of hours"}
         };
 
-        var expectedString = '"Person";"Init hours";"Full hours";"Sum of hours"\n'
+        var expectedString = '"Start Date:";"' + startDate + '";"End Date:";"' + endDate + '"\n'
+            + ';;;\n'
+            + '"Person";"Init hours";"Full hours";"Sum of hours"\n'
             + '"FirstLastName FirstFirstName";"20,5";"20";"20,4"\n'
             + '"SecondLastName SecondFirstName";"0";"10";"2,4"\n';
 
-        var expectedEmptyString = '"Person";"Init hours";"Full hours";"Sum of hours"\n';
+        var expectedEmptyString = '"Start Date:";"' + startDate + '";"End Date:";"' + endDate + '"\n'
+            + ';;;\n'
+            + '"Person";"Init hours";"Full hours";"Sum of hours"\n'
 
         var expectedLink = "data:text/csv;charset=utf-8,";
 
@@ -47,12 +54,12 @@ define(['angular', 'angularMocks', 'application/report/services/tdprReportCsvDat
 
         describe('When generating csv string value', function(){
             it('should return expected string', function(){
-                var dataString = $service.generateCsvData(reports, columnMap);
+                var dataString = $service.generateCsvData(startDate, endDate, reports, columnMap);
                 expect(dataString).toEqual(expectedString);
             });
 
             it('should return expected empty string', function(){
-                var dataString = $service.generateCsvData([], columnMap);
+                var dataString = $service.generateCsvData(startDate, endDate, [], columnMap);
                 expect(dataString).toEqual(expectedEmptyString);
             });
 
