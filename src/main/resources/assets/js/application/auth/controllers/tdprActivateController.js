7b3219d733f020551dc048374306d73d5776c565
Remigiusz Kruk <r.kruk@kainos.com>
2016-08-24 16:33:01 +0200

    US-48 change controller to check if passwords are correct

--- src/main/resources/assets/js/application/auth/controllers/tdprActivateController.js
+++ src/main/resources/assets/js/application/auth/controllers/tdprActivateController.js
@@ -1,25 +1,28 @@
 define(['angular', 'application/auth/tdprAuthModule', 'application/auth/services/tdprActivateService'], function (angular, tdprAuthModule) {
-    tdprAuthModule.controller("tdprActivateController", function ($scope, $stateParams, tdprActivateService, $state) {
+    tdprAuthModule.controller("tdprActivateController", function ($scope, $stateParams, tdprActivateService, $state, Notification) {
 
         var person;
          tdprActivateService.checkIfPersonWithActivationLinkExists($stateParams.activationLink)
             .then(function(response) {
                 person = response.data;
             }, function() {
-               console.log("fail"); // $state.go('tdpr.login')
+                $state.go('tdpr.login');
             });
 
         $scope.activatePerson = function() {
-            var personWithPassword = {
-                id: person.id,
-                password: $scope.password
-            }
-            tdprActivateService.activatePerson(personWithPassword)
-                .then(function() {
-                    console.log("activation successful");
-                }, function() {
-                    console.log("fail");
-                });
+            if ($scope.arePasswordsCorrect) {
+                var personWithPassword = {
+                    id: person.id,
+                    password: $scope.password
+                }
+                tdprActivateService.activatePerson(personWithPassword)
+                    .then(function () {
+                        Notification.success("You have successfully activated your account");
+                        $state.go('tdpr.login');
+                    }, function () {
+                        Notification.error("Something went wrong");
+                    });
+            } else Notification.error("Passwords are not the same or are incorrect");
         }
     });
 });
