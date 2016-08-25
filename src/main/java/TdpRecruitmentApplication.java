1bb06f0f63df76043a5891f86967510da2b5b351
Jakub Raniszewski <j.raniszewski@kainos.com>
2016-08-25 11:11:47 +0200

    US-55: Backend: add resource and class in TdpRecruitmentApplication

--- src/main/java/TdpRecruitmentApplication.java
+++ src/main/java/TdpRecruitmentApplication.java
@@ -15,7 +15,7 @@ public class TdpRecruitmentApplication extends Application<TdpRecruitmentApplica
 
     private GuiceBundle<TdpRecruitmentApplicationConfiguration> guiceBundle;
 
-    private final HibernateBundle<TdpRecruitmentApplicationConfiguration> hibernateBundle = new HibernateBundle<TdpRecruitmentApplicationConfiguration>(AvailabilityType.class, SlotTime.class, Slot.class, Person.class, Note.class, Candidate.class) {
+    private final HibernateBundle<TdpRecruitmentApplicationConfiguration> hibernateBundle = new HibernateBundle<TdpRecruitmentApplicationConfiguration>(AvailabilityType.class, SlotTime.class, Slot.class, Person.class, Note.class, Candidate.class, RecruiterNote.class) {
         @Override
         public DataSourceFactory getDataSourceFactory(TdpRecruitmentApplicationConfiguration configuration) {
             return configuration.getDataSourceFactory();
@@ -54,6 +54,7 @@ public class TdpRecruitmentApplication extends Application<TdpRecruitmentApplica
         environment.jersey().register(guiceBundle.getInjector().getInstance(SlotResource.class));
         environment.jersey().register(guiceBundle.getInjector().getInstance(ReportResource.class));
         environment.jersey().register(guiceBundle.getInjector().getInstance(CandidateResource.class));
+        environment.jersey().register(guiceBundle.getInjector().getInstance(RecruiterNoteResource.class));
 
     }
 
