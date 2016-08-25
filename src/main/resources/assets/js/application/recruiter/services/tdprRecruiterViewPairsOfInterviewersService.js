f362d038611e815582acd764ecd69d3673d9e77d
Sara Cygańska <S.Cyganska@kainos.com>
2016-08-25 09:39:19 +0200

    Change getPairs to always return promise Fixes #137

--- src/main/resources/assets/js/application/recruiter/services/tdprRecruiterViewPairsOfInterviewersService.js
+++ src/main/resources/assets/js/application/recruiter/services/tdprRecruiterViewPairsOfInterviewersService.js
@@ -5,30 +5,25 @@ define(['angular', 'application/recruiter/tdprRecruiterModule'], function (angul
         service.getPairs = function(roles, startDay, endDay, startTime, endTime){
             var pathParams = this.createPathParams(roles, startDay, endDay, startTime, endTime);
             if(!pathParams){
-                return false;
+                return $q.reject("No dates were chosen");
             }
 
             return $http.get('api/pairs?' + pathParams).then(function(response){
                     return response.data;
                 },
                 function(error){
-                    error.message = "Getting data form server failed";
                     return $q.reject(error.message);
                 });
         };
 
         service.createPathParams = function(roles, startDate, endDate, startTime, endTime) {
 
-            if (!roles || !startDate || !endDate || !startTime || !endTime) {
+            if (!startDate || !endDate || !startTime || !endTime || !(startDate instanceof Date && endDate instanceof Date)) {
                 return false;
             }
 
             var startDay =  startDate.getFullYear() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getDate();
             var endDay =  endDate.getFullYear() + "-" + (endDate.getMonth() + 1) + "-" + endDate.getDate();
-
-            if(roles.length == 0 || !roles || !startDay || !endDay){
-                return false;
-            }
 
             var pathParams = "startDate=" + startDay;
             pathParams += "&endDate=" + endDay;
