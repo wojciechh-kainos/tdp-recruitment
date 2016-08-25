5c83a32a263203dcbf442d22f5d16826e018a591
mntss <mateusz.piotrowski94@gmail.com>
2016-08-25 11:10:35 +0200

    Add slots updating and message sending

--- src/main/resources/assets/js/application/recruiter/controllers/tdprCreateEventController.js
+++ src/main/resources/assets/js/application/recruiter/controllers/tdprCreateEventController.js
@@ -1,11 +1,11 @@
 define(['angular', 'application/recruiter/tdprRecruiterModule'
     , 'application/recruiter/services/tdprPersonsService'], function (angular, tdprRecruiterModule) {
-    tdprRecruiterModule.controller("tdprCreateEventController", function ($scope, tdprScheduleService, tdprRecruiterSlotsService, $stateParams, $state) {
+    tdprRecruiterModule.controller("tdprCreateEventController", function ($scope, tdprScheduleService, tdprRecruiterSlotsService, $stateParams, $state, $q) {
 
     console.log($stateParams);
         // var interviewData = $stateParams.data;
         $scope.interview = $stateParams.data.interview;
-
+        $scope.interview.message = "Hi, I scheduled interview for you."
 
         $scope.scheduleInterview = function () {
             $stateParams.data.interview.organizer = { //TODO change to recruiter data
@@ -13,10 +13,17 @@ define(['angular', 'application/recruiter/tdprRecruiterModule'
                 firstName: "Janek",
                 lastName: "Smyk"
             };
-            
-            tdprRecruiterSlotsService.updateSlots($stateParams.data.newSlots).then(function () {
-                tdprScheduleService.sendInvitations($stateParams.data.interview)
+
+            var updateRequests = [];
+            $stateParams.data.interview.interviewers.forEach(function (interviewer) {
+                updateRequests.push(tdprRecruiterSlotsService.updateSlots(interviewer.slots));
+                delete interviewer.slots; //empty to avoid jackson parsing problems
             });
+
+            $q.all(updateRequests).then(function () {
+                tdprScheduleService.sendInvitations($scope.interview)
+            });
+
         };
         
         $scope.goHome = function () {
