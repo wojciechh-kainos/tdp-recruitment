74084b4ada63b74938b23d8bc6dfff42d59c6f4a
Dawid Kwidzinski <dawidkw@gargoyle.local>
2016-08-26 14:51:49 +0200

    remove unnecessary tabs

--- src/main/resources/assets/js/application/auth/controllers/tdprLoginController.js
+++ src/main/resources/assets/js/application/auth/controllers/tdprLoginController.js
@@ -1,20 +1,20 @@
 define(['angular', 'application/auth/tdprAuthModule', 'application/auth/services/tdprAuthService'], function (angular, tdprAuthModule) {
-        tdprAuthModule.controller("tdprLoginController", function ($scope, tdprAuthService, $state, Notification) {
-            $scope.login = function () {
-                tdprAuthService.login($scope.username, $scope.password)
-                    .then(function (user) {
-                       if (user.admin) {
-                            $state.go('tdpr.recruiter.candidates');
-                       } else {
-                            $state.go('tdpr.interviewer.home', {id: user.id});
-                       }
-                    }, function (response) {
-                        if(response.status === 401) {
-                            Notification.error("Wrong email or password.");
-                        } else if (response.status === 403) {
-                            Notification.error("Your account has been disabled.");
-                        }
-                    });
-            };
-        });
+    tdprAuthModule.controller("tdprLoginController", function ($scope, tdprAuthService, $state, Notification) {
+        $scope.login = function () {
+            tdprAuthService.login($scope.username, $scope.password)
+                .then(function (user) {
+                   if (user.admin) {
+                        $state.go('tdpr.recruiter.candidates');
+                   } else {
+                        $state.go('tdpr.interviewer.home', {id: user.id});
+                   }
+                }, function (response) {
+                    if(response.status === 401) {
+                        Notification.error("Wrong email or password.");
+                    } else if (response.status === 403) {
+                        Notification.error("Your account has been disabled.");
+                    }
+                });
+        };
     });
+});
